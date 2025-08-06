9954a7dec935678f8344afbcbb8c3ee2a698deeb
Radha krishna Matta <145093089+RadhakrishnaMATTA@users.noreply.github.com>
2025-08-06 10:32:46 +0530

    Create print_range_odd_even.java

---
+++ print_range_odd_even.java
@@ -0,0 +1,53 @@
+class Main{
+
+public static void main(String args[])
+
+{
+
+for(int i=100; i>=1; i--)
+   {
+      
+    if(i%2==0)
+   System.out.print(" \t"+i);
+  
+
+   }
+
+
+  for(int i =100; i >=1; i--)
+     {
+ 
+      if( i %2 !=0)
+      {
+           
+      System.out.print(" \t "+i);
+       } 
+  
+
+    }
+
+
+  for(int i = 200; i<=400; i++)
+    {
+
+     if(i %2==0)
+     System.out.print(" "+ i);
+
+    }
+
+
+ for(int i = 200; i<=400; i++)
+    {
+
+     if(i % 2!=0)
+     System.out.print(" "+ i);
+
+    }
+
+
+
+
+}
+
+}
+
