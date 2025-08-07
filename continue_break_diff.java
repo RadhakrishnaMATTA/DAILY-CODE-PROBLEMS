d017c5205a903d12da040cd760080a609036b390
Radha krishna Matta <145093089+RadhakrishnaMATTA@users.noreply.github.com>
2025-08-07 15:02:34 +0530

    Create continue_break_diff.java

---
+++ continue_break_diff.java
@@ -0,0 +1,31 @@
+class Main
+
+{
+
+public static void main(String args[])
+{
+
+ int n =5;
+for(int i=0; i <=n ; i++)
+   {
+
+    System.out.println("after break"+i);
+     break;
+
+     }
+  
+  for(int i = 0; i<=n; i++)
+   {
+
+  System.out.println("before continue"+i);
+     continue;
+    }
+    
+
+   
+
+
+
+
+}
+}
