e54f1601757aee6cff5e8154ca6926763614e7ff
Radha krishna Matta <145093089+RadhakrishnaMATTA@users.noreply.github.com>
2025-08-07 15:13:17 +0530

    Create number_difference.java

---
+++ number_difference.java
@@ -0,0 +1,27 @@
+class Main
+
+{
+
+public static void main(String args[])
+{
+    
+    System.out.println("this is diff 567 to 445");
+   for(int i=567; i>=445; i--)
+    {
+
+     System.out.println(i);
+     }
+
+    System.out.println("this is diff 445to 789");
+    for(int i= 445; i<=789; i++)
+    {
++
+   
+    System.out.println(i);
+
+    }
+    
+
+
+}
+}
