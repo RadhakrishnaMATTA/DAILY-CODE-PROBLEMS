31829be2442fdd39aaeced5f3a4bd95ad07aa62e
Radha krishna Matta <145093089+RadhakrishnaMATTA@users.noreply.github.com>
2025-08-11 10:45:49 +0530

    Create array_length.java

---
+++ array_length.java
@@ -0,0 +1,33 @@
+
+class Main {
+    public static void main(String[] args) {
+
+    int arr[] = {139,12,34,5,566,7};
+
+    char ch[] = {'c','3','(','?'};
+    String st[] = {"hello","all","good","morning","453","?<>|+_"};
+    
+    //boolean b[] = {true,false,true,false,ture,true};
+
+    for(int i=0; i<arr.length; i++)
+     {
+
+
+       System.out.println(arr[i]);
+      
+       
+      
+
+    } 
+ 
+    System.out.println("array the length of the array:-"+arr.length);
+         
+
+
+
+
+
+
+         
+    }
+}
