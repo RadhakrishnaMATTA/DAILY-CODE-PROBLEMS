1eb2f07551be2eb7d03f71b85aeaba452521dcb7
Radha krishna Matta <145093089+RadhakrishnaMATTA@users.noreply.github.com>
2025-08-11 09:36:41 +0530

    Create smallest_of_three.java

---
+++ smallest_of_three.java
@@ -0,0 +1,56 @@
+
+
+class Main {
+    public static void main(String[] args) {
+
+    int a = 10, b=0, c=1;
+
+ 
+    if(a<b)
+       {
+
+            if(a<c)
+            {
+
+             System.out.println("a is smallest");
+     
+  
+             }
+            else
+          
+            System.out.println("c is smallerst");
+
+          }
+      else
+           {
+             
+             if(b<c)
+                {
+
+                System.out.println("b is smaller");
+
+                }
+               else
+                {
+                 
+                System.out.println("c is smaller");
+
+                 }
+
+   
+
+
+            }
+ 
+         
+
+       
+
+
+
+
+
+
+         
+    }
+}
