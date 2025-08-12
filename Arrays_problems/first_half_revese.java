2489e317aba9fd8fa7f7f4cdf3f7201a365557a6
Radha krishna Matta <145093089+RadhakrishnaMATTA@users.noreply.github.com>
2025-08-12 22:22:20 +0530

    Create first_half_revese.java

---
+++ Arrays_problems/first_half_revese.java
@@ -0,0 +1,38 @@
+
+class Main {
+    public static void main(String[] args) {
+               
+
+     int arr [] = {1,2,3,4,5,6};
+
+     int mid = (arr.length-1)/2; //3 but the index of 3 is =4 in array 
+
+      for(int i=0; i<=mid; i++)
+     {
+           
+        System.out.print(arr[i]+ " ");
+     } 
+
+    for(int i = mid+1; i<=arr.length-1; i++)
+     {
+
+       
+        System.out.print(arr[i]+ " ");
+     
+
+      }
+
+      
+   }
+
+    
+   }
+
+     
+
+      
+           
+         
+   
+
+
