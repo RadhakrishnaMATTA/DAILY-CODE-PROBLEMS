72608578d1c43a5075e64f92f11519f1484d61c2
Radha krishna Matta <145093089+RadhakrishnaMATTA@users.noreply.github.com>
2025-08-12 23:10:57 +0530

    Create array_element_divisible_4.java

---
+++ Arrays_problems/array_element_divisible_4.java
@@ -0,0 +1,34 @@
+
+class Main {
+    public static void main(String[] args) {
+              
+       
+                 //0 1 2 3 4 5-
+     int arr [] = {1,2,3,4,5,6};
+
+     for(int i=0; i<=arr.length-1; i++)
+
+      {
+
+         if(arr[i]%4==0)
+           {
+
+             System.out.println(arr[i]); 
+         
+           }
+
+    }
+      
+   }
+
+    
+   }
+
+     
+
+      
+           
+         
+   
+
+
