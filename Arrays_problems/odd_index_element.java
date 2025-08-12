7d243601b070b6a704a47669ab592f4d79b17d1e
Radha krishna Matta <145093089+RadhakrishnaMATTA@users.noreply.github.com>
2025-08-12 23:08:33 +0530

    Create odd_index_element.java

---
+++ Arrays_problems/odd_index_element.java
@@ -0,0 +1,34 @@
+
+class Main {
+    public static void main(String[] args) {
+              
+       
+                 //0 1 2 3 4 5-
+     int arr [] = {1,2,3,4,5,6};
+
+     for(int i=0; i<=arr.length-1; i++)
+
+      {
+
+         if(i%2!=0)
+           {
+
+             System.out.println(arr[i]); 
+         
+           }
+
+    }
+      
+   }
+
+    
+   }
+
+     
+
+      
+           
+         
+   
+
+
