0a9393a87c5a15a699eb04b235e3cfaa4bcab202
Radha krishna Matta <145093089+RadhakrishnaMATTA@users.noreply.github.com>
2025-08-13 22:32:31 +0530

    Create smallest_value_array.java

---
+++ Arrays_problems/smallest_value_array.java
@@ -0,0 +1,28 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+class Main {
+    public static void main(String[] args) {
+       
+       int arr[] ={1,2,3,4,5,5};
+       int min=arr[0];
+       for(int i=1 ; i<arr.length; i++)
+        {
+            
+            
+            if( arr[i] < min) //2<1,3<1,4<1,0<1 0<1
+              {
+                  
+                  arr[i]=min;
+                  
+              }
+            
+        }
+       
+            System.out.println(min);
+      
+        
+       
+       
+    }
+}
