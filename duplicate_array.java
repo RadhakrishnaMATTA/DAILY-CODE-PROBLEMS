be9da41b98027a1667263a355b475e920691633c
Radha krishna Matta <145093089+RadhakrishnaMATTA@users.noreply.github.com>
2025-08-18 18:27:48 +0530

    Create duplicate_array.java

---
+++ duplicate_array.java
@@ -0,0 +1,40 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+class Main {
+    public static void main(String[] args) {
+       int arr[] ={1,2,3,4,5,6,7,8,9,1,2,4};
+       
+       boolean b[] = new boolean[arr.length];
+       
+       for(int i=0; i< arr.length; i++)// fix the element
+       {
+           
+           if(b[i]==true)
+           {
+               
+               continue;
+           }
+           
+           int count =1;
+            for(int j=i+1; j<arr.length; j++)
+            {
+                
+               if(arr[i] == arr[j])
+               {
+                   count++;
+                   b[j]= true;
+                   
+               }
+                
+            }
+            
+           
+            if(count>1)
+              System.out.println("frequency is "+ arr[i] + "===> "+count);
+          
+           
+       }
+       
+    }
+}
