77ecfcb8cc2e8333cad4224fd1eda8ca9a213d68
Radha Krishna Matta <radhakrishnamatta5757@gmail.com>
2025-08-21 22:46:25 +0530

    hi

---
+++ simple.java
@@ -0,0 +1,65 @@
+class main {
+
+public static void main(String args[])
+
+
+
+{
+
+  //explict data type
+
+
+    double d = 10.0;
+
+/*
+long big = 10000L;
+// int i = big;             // ❌ long → int
+
+double d = 99.99;
+// float f = d;             // ❌ double → float
+
+float f1 = 5.5f;
+// int i = f1;              // ❌ float → int
+
+int x = 130;
+// byte b = x;              // ❌ int → byte
+
+char c = 'A';
+// byte b = c;              // ❌ char → byte
+
+double amount = 150.75;
+// long l = amount;         // ❌ double → long
+  
+
+*/
+
+
+
+  long l = 1000;
+  float f = l ;
+  
+
+System.out.println(f);
+
+
+
+
+
+
+
+  
+
+
+System.out.println("hlo");
+
+  
+
+
+
+
+
+
+}
+
+
+   }
