1be9ca5faa61c741eaabdb97bf881957d401640b
Radha Krishna Matta <radhakrishnamatta5757@gmail.com>
2025-08-26 12:43:13 +0530

    2

---
+++ String/reverse_string.java
@@ -0,0 +1,21 @@
+class Main
+
+{
+
+public static void main(String args[])
+
+ {
+   
+
+String s = new String("hiarehoware");
+ 
+StringBuffer sb = new StringBuffer(s);
+
+sb.reverse();
+
+System.out.println(sb); 
+  
+ 
+}
+
+}
