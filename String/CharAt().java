c3db3947abeff3b88a26d9f28419785820b4c3ee
Radha krishna Matta <145093089+RadhakrishnaMATTA@users.noreply.github.com>
2025-10-10 08:56:32 +0530

    Create CharAt().java

---
+++ String/CharAt().java
@@ -0,0 +1,22 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+class Main {
+    public static void main(String[] args) {
+        System.out.println("Try programiz.pro");
+        
+     String   s ="radha";
+     
+     String  s1 = "radha";
+     String s3 = "krishna";
+     
+     
+     
+     for(int i=0;i<s.length();i++){
+         
+         System.out.println(s.charAt(i));
+     }
+     
+     
+    }
+}
